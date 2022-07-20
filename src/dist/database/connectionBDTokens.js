20e66fb4ca7e40e9701899348526c56fb4222e90
Chepe05 <joseleiton0511@gamil.com>
2022-07-20 15:10:34 -0600

    connections strings empty

--- src/dist/database/connectionBDTokens.js
+++ src/dist/database/connectionBDTokens.js
@@ -1,14 +1,16 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
+var __awaiter = (this && this.__awaiter) || function(thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function(resolve) { resolve(value); }); }
+    return new(P || (P = Promise))(function(resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+
         function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
+var __importDefault = (this && this.__importDefault) || function(mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
@@ -18,20 +20,19 @@ const config_1 = __importDefault(require("../config"));
 const dbSettings = {
 
 
-    user: 'db_a89ace_ciiadheltoken_admin',
-    password: 'Nuvian123',
-    server: 'SQL8002.site4now.net',
-    database: 'db_a89ace_ciiadheltoken',
+    user: '',
+    password: '',
+    server: '',
+    database: '',
     options: {
         encrypt: true,
         trustServerCertificate: true // change to true for local dev / self-signed certs
     }
 };
-const getConnetionTokens = () => __awaiter(void 0, void 0, void 0, function* () {
+const getConnetionTokens = () => __awaiter(void 0, void 0, void 0, function*() {
     try {
         return yield mssql_1.default.connect(dbSettings);
-    }
-    catch (error) {
+    } catch (error) {
         throw new Error(error);
     }
 });
